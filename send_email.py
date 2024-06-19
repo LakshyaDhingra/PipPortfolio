9083a008edc1ab4fffc036a311d58384e3e1ef02
Lakshya Dhingra <dlakshya223@gmail.com>
2024-06-19 15:16:37 +0530

    Completing App 2

--- send_email.py
+++ send_email.py
@@ -13,6 +13,7 @@ def emailsend(message):
     my_context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.set_debuglevel(1)
         server.login(username, password)
         server.sendmail(username, receiver, message)
 
