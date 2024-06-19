5730a311005298fca21f3a78402c892f0a2236d2
Lakshya Dhingra <dlakshya223@gmail.com>
2024-06-19 15:53:56 +0530

    Completing App 2

--- send_email.py
+++ send_email.py
@@ -6,9 +6,9 @@ def emailsend(message):
     host = "smtp.gmail.com"
     port = 465
 
-    password = os.environ.get("PASSWORD")
-    username = os.environ.get("EMAIL")
-    receiver = os.environ.get("EMAIL2")
+    username = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+    receiver = os.getenv("EMAIL2")
     my_context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=my_context) as server:
