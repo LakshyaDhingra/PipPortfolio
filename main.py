494ca9970e013fb2994222d5522cfac351f04f40
Lakshya Dhingra <dlakshya223@gmail.com>
2024-06-19 12:53:43 +0530

    Render the app titles

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
+
 with col1:
     st.image("images/photo.png", width=400)
 with col2:
@@ -14,6 +16,17 @@ with col2:
     st.info(content)
 
 content2 = """
-Below you can find some of the apps built by me in Python, feel free to contact me
+Below you can find some of the apps built by me in Python, feel free to contact me!
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
